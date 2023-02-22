2ca4923a5548612fd54ba55dfd2af991151ed07f
Ross Merrigan <email@rossmerrigan.com>
2023-02-22 23:57:22 +0100

    cleanup fm-index

--- fmindex.go
+++ fmindex.go
@@ -2,7 +2,6 @@ package fmindex
 
 import (
 	"fmt"
-	"sort"
 	"strings"
 
 	"github.com/rossmerr/bwt"
@@ -67,7 +66,8 @@ func (s *FMIndex) Extract(offset, length int) string {
 }
 
 func (s *FMIndex) Count(pattern string) int {
-	return 0
+	f, l := s.query(pattern)
+	return l - f
 }
 
 func (s *FMIndex) Locate(pattern string) int {
@@ -75,130 +75,33 @@ func (s *FMIndex) Locate(pattern string) int {
 
 }
 
-// LF Mapping, look at the last column of the BWT matrix and the follow it to the first column
-func (s *FMIndex) lf(c rune, index int) int {
-	rank := s.l.Rank(c, index)
-	return s.f.Select(c, rank)
-}
-
-func (s *FMIndex) Search(pattern string) *FMIndexResult {
+func (s *FMIndex) query(pattern string) (top, bottom int) {
 	if s.caseinsensitive {
 		pattern = strings.ToUpper(pattern)
 	}
 
-	occurrences := s.firstOccurrences(pattern)
-
 	length := len(pattern)
-	count := 0
 
-	for p := length - 1; p > 0; p-- {
-		next := rune(pattern[p-1])
+	// // look at the pattern in reverse order
+	next := rune(pattern[length-1])
+
+	top = s.f.Select(next, 0)
+	bottom = s.f.Select(next, s.l.Length()) + 1
+
+	i := length - 2
+	for i >= 0 && bottom > top {
+		next = rune(pattern[i])
 
 		fmt.Println("current " + string(next))
-		toDelete := []int{}
-		for i, arr := range occurrences {
-			index := arr[count]
-
-			if s.l.Access(index) == next {
-				occurrences[i] = append(arr, s.lf(next, index))
-			} else {
-				toDelete = append(toDelete, i)
-			}
-
-		}
-		sort.Ints(toDelete)
-
-		for i := len(toDelete) - 1; i >= 0; i-- {
-			o := toDelete[i]
-			occurrences = append(occurrences[:o], occurrences[o+1:]...)
-		}
-
-		count++
+		n1 := s.l.Rank(next, top)
+		n2 := s.l.Rank(next, bottom)
+		fmt.Println(n1)
+		fmt.Println(n2)
+		skip := s.f.Select(next, 0)
+		top = (n1 + skip)
+		bottom = (n2 + skip)
+		i--
 	}
 
-	result := &FMIndexResult{
-		pattern: pattern,
-		first:   rune(pattern[len(pattern)-1]),
-		index:   s,
-		count:   len(occurrences),
-	}
-
-	rows := []*FMIndexRoweRsult{}
-	for _, arr := range occurrences {
-		rows = append(rows, &FMIndexRoweRsult{
-			access: arr,
-			result: result,
-		})
-	}
-
-	result.rows = rows
-
-	return result
-}
-
-func (s *FMIndex) firstOccurrences(pattern string) [][]int {
-	result := [][]int{}
-	length := len(pattern)
-
-	// look at the pattern in reverse order
-	last := rune(pattern[length-1])
-
-	matching := false
-
-	// skip rows in the first BWT until you reach the last rune from the pattern
-	start := s.f.Select(last, 0)
-	for i := start; i < s.f.Length(); i++ {
-		next := s.f.Access(i)
-		if next == last {
-			result = append(result, []int{i})
-			matching = true
-		} else if (next != last) && matching {
-			break
-		}
-	}
-
-	return result
-}
-
-type FMIndexResult struct {
-	pattern string
-	first   rune
-	index   *FMIndex
-	count   int
-	rows    []*FMIndexRoweRsult
-}
-
-func (s *FMIndexResult) Count() int {
-	return s.count
-}
-
-func (s *FMIndexResult) Rows() []*FMIndexRoweRsult {
-	return s.rows
-}
-
-type FMIndexRoweRsult struct {
-	result *FMIndexResult
-	access []int
-}
-
-// func (s *FMIndexRoweRsult) Offset() int {
-// 	first := s.access[len(s.access)-1]
-
-// 	r := s.result.index.first.Rank(s.result.first, first)
-
-// 	fmt.Println(r)
-
-// 	sel := s.result.index.first.Select(s.result.first, r)
-// 	fmt.Println(sel)
-
-// 	return first
-// }
-
-func (s *FMIndexRoweRsult) String() string {
-	str := ""
-	for i := len(s.access) - 1; i >= 0; i-- {
-		str += string(s.result.index.f.Access(s.access[i]))
-	}
-
-	return str
+	return
 }
--- fmindex_test.go
+++ fmindex_test.go
@@ -12,40 +12,26 @@ func TestFMIndex_Search(t *testing.T) {
 		text    string
 		pattern string
 		count   int
-		offset  []int
 	}{
-		// {
-		// 	name:    "abaaba",
-		// 	text:    "abaaba",
-		// 	pattern: "aba",
-		// 	count:   2,
-		// 	offset:  []int{0, 4},
-		// },
+		{
+			name:    "abaaba",
+			text:    "abaaba",
+			pattern: "aba",
+			count:   2,
+		},
 		{
 			name:    "abaaba",
 			text:    "abaaba",
 			pattern: "baa",
 			count:   1,
-			offset:  []int{1},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s, _ := NewFMIndex(tt.text)
-			got := s.Search(tt.pattern)
-			if !reflect.DeepEqual(got.Count(), tt.count) {
-				t.Errorf("FMIndexRoweRsult.String() = %v, want %v", got.Count(), tt.count)
-			}
-			for _, r := range got.Rows() {
-				str := r.String()
-				if !reflect.DeepEqual(str, tt.pattern) {
-					t.Errorf("FMIndexRoweRsult.String() = %v, want %v", str, tt.pattern)
-				}
-
-				// offset := r.Offset()
-				// if !reflect.DeepEqual(offset, tt.offset[i]) {
-				// 	t.Errorf("FMIndexRoweRsult.Offset() = %v, want %v", offset, tt.offset[i])
-				// }
+			got := s.Count(tt.pattern)
+			if !reflect.DeepEqual(got, tt.count) {
+				t.Errorf("FMIndex.Count() = %v, want %v", got, tt.count)
 			}
 		})
 	}
